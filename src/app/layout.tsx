9d2fe9d7c0ef110cfb1e2ab2c52100fa23c7c4fe
Fn-front <reservoir-station@outlook.jp>
2024-10-01 20:29:36 +0900

    add: page pwa設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from 'next/font/google';
 import { Inter } from "next/font/google";
+import type { Viewport } from 'next'
 
 import '@/styles/app.scss'
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  themeColor: '#000000',
+}
 
 const noto = Noto_Sans_JP({
   weight: ["400", "700"],
@@ -15,6 +20,8 @@ const noto = Noto_Sans_JP({
 export const metadata: Metadata = {
   title: "テンプレ",
   description: "テンプレ",
+  manifest: '/manifest.json',
+  icons: '/icons/icon-192x192.png'
 };
 
 export default function RootLayout({
