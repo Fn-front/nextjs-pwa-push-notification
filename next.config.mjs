9856bad70c107f7f9c9e4b54c18270b6c5b5e4e7
Fn-front <reservoir-station@outlook.jp>
2024-10-01 20:15:09 +0900

    add: next.configにpwaの設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  dest: 'public',  // Service Worker や PWA 関連ファイルを配置する場所
+  disable: process.env.NODE_ENV === 'development', // 開発環境ではPWAを無効にする
+});
 
 export default nextConfig;
