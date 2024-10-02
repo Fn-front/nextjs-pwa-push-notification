e4fea0c611a00fea802c5fcc7100005d9026439f
Fn-front <reservoir-station@outlook.jp>
2024-10-02 13:32:34 +0900

    add: pwa編集

--- src/features/pwa/index.tsx
+++ src/features/pwa/index.tsx
@@ -5,8 +5,8 @@ import { useEffect } from 'react';
 export const Pwa = () => {
   useEffect(() => {
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
+      navigator.serviceWorker.register('/worker/index.js').then(registration => {
+        console.log('Service Worker registered with scope:', registration);
       }).catch(error => {
         console.error('Service Worker registration failed:', error);
       });
